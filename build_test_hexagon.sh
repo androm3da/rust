b4ee4350097bbcc5d8bcd42dce177d5bebec0c79
Brian Cain <bcain@quicinc.com>
2024-04-26 19:35:37 -0700

    Add build script, config
    Fix the RUNNER env var in the test script
    
    Signed-off-by: Brian Cain <bcain@quicinc.com>
    
    Use a runner to invoke qemu-hexagon for tests
    
    Set the QEMU_LD_PREFIX
    
    This was causing failures in test cases when they forked new processes.  The
    children would invoke QEMU but without the context necessary to find the C
    library.  The tests would fail with an error message like this:
    
        qemu-hexagon: Could not open '/lib/ld-musl-hexagon.so.1': No such file or directory
    
    Current test results:
    
        $ rg '^test result' 16276_pass_138_fail.log
        151934:test result: ok. 16246 passed; 0 failed; 514 ignored; 0 measured; 0 filtered out; finished in 88.39s
        161336:test result: FAILED. 16276 passed; 138 failed; 346 ignored; 0 measured; 0 filtered out; finished in 500.53s
    
    Signed-off-by: Brian Cain <bcain@quicinc.com>
    
    cfg: fix the runner

---
+++ build_test_hexagon.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -ex
+
+export CC_hexagon_unknown_linux_musl=hexagon-linux-musl-clang
+export CC_hexagon_unknown_none_elf=hexagon-unknown-none-elf-clang
+
+# Updated to use the current hexagon linux toolchain
+export PATH=${PATH}:/opt/clang+llvm-20.1.4-cross-hexagon-unknown-linux-musl/x86_64-linux-gnu/bin
+
+export QEMU_LD_PREFIX=/opt/clang+llvm-20.1.4-cross-hexagon-unknown-linux-musl/x86_64-linux-gnu/target/hexagon-linux-musl/usr
+# Per-test-case timeout: 300 seconds (5 minutes) for individual test executables
+export CARGO_TARGET_HEXAGON_UNKNOWN_LINUX_MUSL_RUNNER="timeout 300s qemu-hexagon -L ${QEMU_LD_PREFIX}"
+
+#/pkg/qct/software/llvm/build_tools/clang+llvm-16.0.0-cross-hexagon-unknown-linux-musl/x86_64-linux-gnu/target/hexagon-unknown-linux-musl/
+# hack to get around bug, during bootstrap, something
+# throws out $CFG_DEFAULT_LINKER from the env and rustc
+# tries to use `cc`.
+#ln -sf /pkg/qct/software/gnu/gcc/6.3.0/bin/g++ cc
+
+export CC=gcc
+export CXX=g++
+export CC=clang
+export CXX=clang++
+#export LD=g++
+#export LD=/pkg/qct/software/gnu/gcc/6.3.0/bin/g++
+export CFG_DEFAULT_LINKER=clang++
+export RUST_BACKTRACE=1
+
+bsub_() {
+#   bsub -I -o run_$(date +"%d_%b_%H%M%S").log -R 'select[ubuntu20_llvm] && rusage[mem=12288]' $*
+    \time $*
+}
+
+build() {
+    bsub_ ./x.py build \
+        --stage 1 \
+        -vv \
+        --config config_hex_linux_and_unknown.toml \
+
+    bsub_ ./x.py dist \
+        --stage 1 \
+        -vv \
+        --config config_hex_linux_and_unknown.toml \
+
+    bsub_ ./x.py install \
+        --stage 1 \
+        -vv \
+        --config config_hex_linux_and_unknown.toml \
+
+}
+
+run_tests() {
+    bsub_ ./x.py test \
+        --stage 1 \
+        -vv \
+        --config config_hex_linux_and_unknown.toml \
+        --exclude src/tools/tidy
+
+}
+
+set -euo pipefail
+
+build 2>&1 | tee build.log
+run_tests 2>&1 | tee test.log
+
